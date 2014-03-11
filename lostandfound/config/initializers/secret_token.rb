e080d17386bc1eddd9a2d95712c5fd264af1aa36
alex <apomata@brandeis.edu>
2014-03-11 00:06:31 -0400

    first commit
    lostandfound all set up

---
+++ lostandfound/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostandfound::Application.config.secret_key_base = '2ec9ad76a78ad0570a50727525e1db7e90860138e5b7858be138b613cbf83c9166f4a7e89f170191f3857b9e46012c53d8aa56a46819586616df80265894ed3b'
